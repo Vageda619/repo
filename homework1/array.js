639d0dbcd70c12b7337c651da9f06ef4da434ca0
Vageda619 <anpilogovartyom@gmail.com>
2024-02-24 03:35:32 +0300

    сделал дз6

--- homework1/array.js
+++ homework1/array.js
@@ -6,7 +6,7 @@ for (const el of arr) {
         break;
     }
     console.log(el);
-}*/
+}
 
 
 const arr = [1, 5, 4, 10, 0, 3];
@@ -25,3 +25,100 @@ const joinedString = numbers.join(' ');
 console.log(joinedString);
 
 
+const rows = 3;
+const cols = 3;
+const value = 1;
+const multiArray = [];
+
+for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+        row.push(value);
+    }
+    multiArray.push(row);
+}
+
+console.log(multiArray);
+
+
+
+let array = [1, 1, 1];
+const valuesToAdd = [2, 2, 2];
+
+array.push(...valuesToAdd);
+
+console.log(array);
+
+
+
+let array = [9, 8, 7, 'a', 6, 5];
+
+array.sort();
+
+array = array.filter(item => typeof item === 'number');
+
+console.log(array);
+
+
+const numbers = [9, 8, 7, 6, 5];
+const userGuess = prompt("Попробуйте угадать число");
+
+if (numbers.includes(Number(userGuess))) {
+    alert("Угадал! 🎉");
+} else {
+    alert("Не угадал... 😕");
+}
+
+
+const str = 'abcdef';
+const reversedStr = str.split('').reverse().join('');
+
+console.log(reversedStr);
+
+
+const arr = [[1, 2, 3], [4, 5, 6]];
+const flatArr = arr.flat();
+
+console.log(flatArr);
+
+
+const array = [2, 5, 8, 3, 7, 1, 4, 6, 9, 10];
+
+for (let i = 0; i < array.length - 1; i++) {
+    const currentElement = array[i];
+    const nextElement = array[i + 1];
+    const sum = currentElement + nextElement;
+    
+    console.log(`Сумма элементов ${currentElement} и ${nextElement} равна: ${sum}`);
+}
+
+
+function squareNumbers(arr) {
+    const squaredArray = arr.map(number => number * number);
+    return squaredArray;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = squareNumbers(numbers);
+
+console.log(squaredNumbers);
+
+
+function getLengthWords(arr) {
+    const lengthsArray = arr.map(word => word.length);
+    return lengthsArray;
+}
+
+const words = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+const wordLengths = getLengthWords(words);
+
+console.log(wordLengths);
+*/
+
+
+function filterPositive(array) {
+    return array.filter(num => num < 0);
+}
+
+console.log(filterPositive([-1, 0, 5, -10, 56]));
+console.log(filterPositive([-25, 25, 0, -1000, -2]));
